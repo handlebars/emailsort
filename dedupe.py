9cc12ec1b60e86924c03eb2614cac514ab932192
Andrew Bae <andrewbae@gmail.com>
2024-01-10 16:23:09 -0800

    Updated script, seems to work, but slooooow

--- dedupe.py
+++ dedupe.py
@@ -1,73 +1,32 @@
 # Python script to dedupe email addresses in a list
+# The text file's list of emails must end with an empty line (just a new line char)
 
-datafile = open('list.txt', 'r')
-outputfile = open('output.txt', 'w')
-
-'''
-for line in datafile.readlines():
-    if line == "karina.hammes@turner.com\n":
-        outputfile.write(line)
-        print("found it")
-    #print(line, end='')
-'''
-
-'''
-outputlist = []
-for line in datafile.readlines():
-    if line == "karina.hammes@turner.com\n":
-        outputlist.append(line)
-    #print(line, end='')
-print(outputlist)
-'''
-
-'''
-outputlist = ["karina.hammes@turner.com\n"]
-#print(outputlist)
-for line in datafile.readlines():
-    for listitem in outputlist:
-        #print(listitem)
-        if listitem == line:
-            print("It already exists, do nothing")
-        else:
-            print("Adding it to output")
-            print(line)
-            outputlist.append(line)
-            break
-'''
+inputfile = open('list.txt', 'r') # The input file (sorry about hardcoded filename)
+outputfile = open('output.txt', 'w') # The output file
 
 outputlist = []
 removedupe = False
-for line in datafile.readlines():
-    #print(line)
-    #print("outputlist: " + str(outputlist))
-    for listitem in outputlist:
-        #print(listitem)
-        if listitem == line:
-            print("It already exists, do nothing")
-            print(removedupe)
+
+for line in inputfile.readlines(): # load input list
+    # Check the output file being constructed to find out if
+    # the email address is already there; if it is, do not add it.
+    for outputlistitem in outputlist:
+        if outputlistitem == line:
+            # The email address is already in the output file, so
+            # do not add it and break out of the inner for loop
             removedupe = True
-            break
+            break # Breaks out of inner for loop
     if removedupe:
-        removedupe = False  #reset removedupe
+        # Reset removedupe signal
+        removedupe = False
     else:
+        # Add the email address to the output list
         outputlist.append(line)
-        print("Adding it to output")
-        print(line)
 
-print('======')
-print(outputlist)
-
+# Send the de-duped list of emails to the output text file
 for outputline in outputlist:
     outputfile.write(outputline)
 
-'''
-for line1 in datafile.readlines():
-    for line2 in outputfile.readlines():
-        if line1 == "karina.hammes@turner.com\n":
-            outputfile.write(line)
-            print("found it")
-    #print(line, end='')
-'''
-
-datafile.close()
+# File handling housekeeping
+inputfile.close()
 outputfile.close()
